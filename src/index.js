d9bd35f439f7618ff80d96dcce3b41ff844b1001
Nazreen <nxmohamad@gmail.com>
2018-04-19 00:26:43 +0800

    create DATE_KEY variable

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 var csv = require("fast-csv");
 
+const DATE_KEY='date'; // todo: set this in .env. use dotenv
+
 !async function () {
     const stream = fs.createReadStream("data/raw.csv");
     var writeStream = fs.createWriteStream("data/result.csv");
@@ -9,10 +11,15 @@ var csv = require("fast-csv");
     csv
         .fromStream(stream, { headers: true })
         .on("data", function (data) {
-            counter++;
-            const ISOdate = new Date(data.date).toISOString();
-            const updatedObject = Object.assign(data, { date: ISOdate });
-            objectsArray.push(updatedObject);
+            try {
+                counter++;
+                const ISOdate = new Date(data[DATE_KEY]).toISOString();
+                const updatedObject = Object.assign(data, { date: ISOdate });
+                objectsArray.push(updatedObject);
+            } catch (e) {
+                console.error(`Error for ${JSON.stringify(data)}`)
+            }
+
         })
         .on("end", function () {
             csv
